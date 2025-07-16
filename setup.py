677e116696fe688d30584b34ccce4e891f4b82c4
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-07-16 10:14:46 -0400

    ..

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cndviz",
+    name="spytial",
     version="0.1",
     packages=find_packages(),
     install_requires=[],
     author="Your Name",
-    description="CnD-compatible visualizer for Python data structures",
+    description="sPyTial: Spatial Python visualization with declarative constraints",
 )
