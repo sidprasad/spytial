2f20c8fa5efdb69baf7f15414fd22108f4fbf380
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-09-07 12:53:06 -0400

    .

--- spytial/domain_relationalizers/primitive_relationalizer.py
+++ spytial/domain_relationalizers/primitive_relationalizer.py
@@ -11,13 +11,7 @@ class PrimitiveRelationalizer(RelationalizerBase):
         return isinstance(obj, (int, float, str, bool, type(None)))
 
     def relationalize(self, obj: Any, walker_func) -> Tuple[List[Atom], List[Relation]]:
-        # Wrap all strings in quotes for clarity
-        if isinstance(obj, str):
-            label = f'"{obj}"'
-        else:
-            label = str(obj)
-        
         atom = Atom(
-            id=walker_func._get_id(obj), type=type(obj).__name__, label=label
+            id=walker_func._get_id(obj), type=type(obj).__name__, label=str(obj)
         )
         return [atom], []
