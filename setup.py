69c20df6904a508ebd2311075b8f05fbac1484d0
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-10-16 10:54:10 -0400

    V uptick

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_readme():
 
 setup(
     name="spytial-diagramming",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=[
         "jinja2>=3.0.0",
